6b0fa2002dcc7bce34b139a5b91b6317c4d47378
Jackson Sanders <madmanjack8@gmail.com>
2026-07-07 10:06:09 -0400

    Create select.tsx

---
+++ components/ui/select.tsx
@@ -0,0 +1,25 @@
+import { forwardRef, type SelectHTMLAttributes } from "react";
+import { ChevronDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { inputClasses } from "./input";
+
+export const Select = forwardRef<
+  HTMLSelectElement,
+  SelectHTMLAttributes<HTMLSelectElement>
+>(function Select({ className, children, ...props }, ref) {
+  return (
+    <div className="relative">
+      <select
+        ref={ref}
+        className={cn(inputClasses, "appearance-none pr-11", className)}
+        {...props}
+      >
+        {children}
+      </select>
+      <ChevronDown
+        className="pointer-events-none absolute right-4 top-1/2 size-5 -translate-y-1/2 text-sand-500"
+        aria-hidden="true"
+      />
+    </div>
+  );
+});
